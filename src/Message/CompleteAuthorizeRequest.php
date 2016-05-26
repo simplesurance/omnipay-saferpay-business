6d4a5ab0cd4c0609f52768ef0d36faed8650d22c
Zoltan Kincses <zoltan.kincses@simplesurance.de>
2016-05-26 13:49:11 +0200

    Updated request params

--- src/Message/CompleteAuthorizeRequest.php
+++ src/Message/CompleteAuthorizeRequest.php
@@ -17,21 +17,26 @@ class CompleteAuthorizeRequest extends AbstractRequest
             'spPassword' => $this->getSpPassword(),
             'AMOUNT' => $this->getAmountInteger(),
             'CURRENCY' => $this->getCurrency(),
-            'NAME' => $this->getCardHolderName(),
-            'IBAN' => $this->getIban(),
-            'MANDATEID' => $this->getMandateId(),
         ];
+
+        // In case of recurring payments we can return right away.
+        if ('yes' === $this->getRecurring()) {
+            return $this->prepareRecurringData($data);
+        }
+
+        $data = array_merge(
+            $data,
+            [
+                'NAME' => $this->getCardHolderName(),
+                'IBAN' => $this->getIban(),
+                'MANDATEID' => $this->getMandateId()
+            ]
+        );
 
         if ($card = $this->getCard()) {
             $data['PAN'] = $card->getNumber();
             $data['EXP'] = $card->getExpiryDate('my');
             $data['CVC'] = $card->getCvv();
-
-            if ('yes' === $this->getInstallment()) {
-                $data['INSTALLMENT']    = 'yes';
-                $data['INSTCOUNT']      = $this->getInstCount();
-                $data['REFID']          = $this->getRefId();
-            }
         } elseif (!$data['IBAN']) {
             $this->validate('bankCode', 'bankAccountNumber');
 
@@ -97,108 +102,57 @@ class CompleteAuthorizeRequest extends AbstractRequest
         return $this->setParameter('bankAccountNumber', $value);
     }
 
-    /**
-     * @return string
-     */
-    public function getInstallment()
+    public function getCardRefId()
     {
-        return $this->getParameter('installment');
+        return $this->getParameter('cardRefId');
     }
 
-    /**
-     * Flags the payment request as installment payment.
-     * Must be set for initial installment payment and
-     * all following installment payments.
-     *
-     * @param   $value    Values: "yes" or "no"
-     *
-     * @return  $this
-     *
-     * @throws  InvalidRequestException
-     */
-    public function setInstallment($value)
+    public function setCardRefId($value)
     {
-        if (!in_array($value, ['yes', 'no'])) {
-            throw new InvalidRequestException(
-                sprintf('Invalid installment value "%s". Allowed values are "yes" and "no".', $value)
-            );
-        }
-
-        return $this->setParameter('installment', $value);
+        return $this->setParameter('cardRefId', $value);
     }
 
-    /**
-     * @return int
-     */
-    public function getInstCount()
+    public function getRecurring()
     {
-        return $this->getParameter('instCount');
+        return $this->getParameter('recurring');
     }
 
-    /**
-     * Number of installments as agreed between merchant and
-     * customer. INSTCOUNT is mandatory for the initial
-     * installment payment and not necessary for following
-     * installment payments!
-     *
-     * @param   $value  Minimum value is "2"
-     *
-     * @return  $this
-     *
-     * @throws  InvalidRequestException
-     */
-    public function setInstCount($value)
+    public function setRecurring($value)
     {
-        if (!is_int($value) || $value < 2) {
-            throw new InvalidRequestException(
-                sprintf('Invalid instCount value "%s". The value must be an "integer" and at least "2".', $value)
-            );
-        }
-
-        return $this->setParameter('instCount', $value);
+        return $this->setParameter('recurring', $value);
     }
 
-    /**
-     * @return string
-     */
     public function getRefId()
     {
         return $this->getParameter('refId');
     }
 
-    /**
-     * Uses the transaction identifier of the initial payment to refer to
-     * following recurring or installment payments.
-     *
-     * @param   $value  ID of the initial payment
-     *
-     * @return  $this
-     */
     public function setRefId($value)
     {
         return $this->setParameter('refId', $value);
     }
 
-    public function getRefOid()
-    {
-        return $this->getParameter('refOid');
-    }
-
-    /**
-     * Uses the reference number of the initial payment to refer to
-     * following recurring or installment payments.
-     *
-     * @param   $value  ORDERID of the initial payment
-     *
-     * @return  $this
-     */
-    public function setRefOid($value)
-    {
-        return $this->setParameter('refOid', $value);
-    }
-
     protected function createResponse($response)
     {
         return new CompleteAuthorizeResponse($this, $response);
+    }
+
+    /**
+     * Returns the required data for recurring payments.
+     *
+     * @param array $data
+     *
+     * @return array
+     */
+    protected function prepareRecurringData(array $data)
+    {
+        return array_merge(
+            $data,
+            [
+                'RECURRING' => 'YES',
+                'REFID' => $this->getRefId(),
+                'CARDREFID' => $this->getCardRefId()
+            ]
+        );
     }
 }
